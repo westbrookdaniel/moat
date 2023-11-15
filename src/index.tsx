11330c772fa22d2fd3dde81c4b56d5bed53d3fe2
westy12dan <westy12dan@gmail.com>
2023-11-15 17:10:35 +0800

    more free

--- src/index.tsx
+++ src/index.tsx
@@ -1,31 +1,15 @@
 import path from "path";
 // @ts-ignore
-import { VNode, __INTERNALS, render } from "@westbrookdaniel/palm";
-import { ComponentType } from "@westbrookdaniel/palm/jsx-runtime";
+import { VNode, __INTERNALS } from "@westbrookdaniel/palm";
 
 export * from "./build";
 
 export const clientEntryMap = new Map();
 
-/**
- * @example
- * export function MyComponent() {
- *   return <div data-component={id}>Hello World</div>;
- * }
- *
- * register(MyComponent, './MyComponent.tsx');
- */
-export function register(Component: ComponentType, filepath: string) {
+export function include(filepath: string) {
   const src = path.join("/_client", filepath);
   const p = src.replace(/\.(ts|tsx|jsx)$/, ".js");
-
-  if (typeof window !== "undefined") {
-    render;
-    Component;
-    p;
-  } else {
-    clientEntryMap.set(filepath, p);
-  }
+  clientEntryMap.set(filepath, p);
 }
 
 export type Handler = (req: Request) => Promise<Response | VNode>;
